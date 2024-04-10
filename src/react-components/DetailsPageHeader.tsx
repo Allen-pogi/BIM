811274207088cb3f78da1e0ee4038c46cce6fa4e
MARIA ROCHELLE R. QUELONIO <queloniomariarochelle@gmail.com>
2024-04-10 13:10:20 +0800

    Update DetailsPageHeader.tsx

--- src/react-components/DetailsPageHeader.tsx
+++ src/react-components/DetailsPageHeader.tsx
@@ -14,26 +14,30 @@ import {
 } from "../class/projects";
 import { ProjectsManager } from "../class/projectsManager";
 
+// Define Props interface to describe the props expected by the DetailsPageHeader component
 interface Props {
-    project: Project
+    project: Project // Project object passed as prop
   }  
 
+// DetailsPageHeader Function Component
 export function DetailsPageHeader(props: Props) {
-    const projectsManager = new ProjectsManager();
+    const projectsManager = new ProjectsManager(); // Instantiate ProjectsManager to manage projects
 
     // Event listener for exporting projects to JSON
     const onExportProjects = () => {
-        projectsManager.exportToJSON();
+        projectsManager.exportToJSON(); // Call exportToJSON method of ProjectsManager
     }
     // Event listener for exporting projects to JSON
     const onImportProjects = () => {
-        projectsManager.importFromJSON()
+        projectsManager.importFromJSON() // Call importFromJSON method of ProjectsManager
     }
 
+    // Event listener for creating a new project
     const onNewProject = () => {
-        toggleModal("new-project-modal");
+        toggleModal("new-project-modal"); // Toggle modal for creating a new project
     };
 
+    // JSX return: Header section of the details page
     return (
         <header>
             <div>
@@ -45,13 +49,16 @@ export function DetailsPageHeader(props: Props) {
             >{props.project.projectDescription}</p>
             </div>
             <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", gap: "8px" }}>
+            {/* Button to create a new project */}
             <button onClick={onNewProject} id="import-projects-btn">
                 <p>Create Project</p>
                 
             </button>
+            {/* Button to export projects */}
             <button onClick={onExportProjects} id="export-projects-btn">
                 <p>Export</p>
             </button>
+            {/* Button to import projects */}
             <button onClick={onImportProjects} id="import-projects-btn">
                 <p>Import</p>
             </button>
