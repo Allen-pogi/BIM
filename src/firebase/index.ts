7994234f649096b4749fa6e6160c9485f0a04b8c
MARIA ROCHELLE R. QUELONIO <queloniomariarochelle@gmail.com>
2024-04-10 12:35:17 +0800

    Update index.ts

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,6 +1,8 @@
+// Import necessary Firebase modules
 import * as Firestore from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 
+// Configuration object for Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBfD-yeqFortwzYmrUMcQFZRstig5E9Fbw",
   authDomain: "that-open-master-challenge.firebaseapp.com",
@@ -10,19 +12,25 @@ const firebaseConfig = {
   appId: "1:583262808832:web:83dc929d0d80f84258e1e5"
 };
 
+// Initialize Firebase app with the provided configuration
 const app = initializeApp(firebaseConfig);
+// Get a reference to the Firestore database
 export const firestoreDB = Firestore.getFirestore()
 
+// Function to get a collection from Firestore database
 export function getCollection<T>(path: string) {
   return Firestore.collection(firestoreDB, path) as Firestore.CollectionReference<T>
 }
+// Function to get a specific document (project) from Firestore
 export function getProject(path: string , id: string) {
   return Firestore.doc(firestoreDB, `${path}/${id}`);
  
 
 }
-
+// Function to delete a document from Firestore
 export function deleteDocument(path: string, id: string) {
+  // Get a reference to the document to be deleted
   const doc = Firestore.doc(firestoreDB,  `${path}/${id}`)
+   // Delete the document from Firestore
   Firestore.deleteDoc(doc)
 }
